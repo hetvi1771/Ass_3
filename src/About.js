92c956be1bb67115de4e468f7b61759791913c51
Hetvi <hetvilad.it22@scet.ac.in>
2025-01-27 15:10:44 +0530

    commit

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>👋Welcome to the About Page by Hetvi</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Home = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <p>👋Welcome to the Home Page by Hevi</p>
+        </div>
+    );
+}
+export default Home;
